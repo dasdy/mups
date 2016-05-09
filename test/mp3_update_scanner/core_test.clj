98266c1abb1faf84d7d51e45257697bceece419c
Podze Aleksandr <dasdeg@gmail.com>
2016-05-09 17:14:43 +0300

    fixed test

--- test/mp3_update_scanner/core_test.clj
+++ test/mp3_update_scanner/core_test.clj
@@ -142,7 +142,7 @@
     (with-local-vars [ file-buf nil]
       (with-redefs [spit (fn [path str] (var-set file-buf str))]
         (do (save-collection {"a" {"b" 1}} "some-path")
-            (is (= @file-buf "{\n  \"a\" : [ \"b\" ]\n}"))))))
+            (is (= @file-buf "{\n  \"a\" : [\n    \"b\"\n  ]\n}"))))))
   (testing "read-collection"
     (with-redefs [slurp (fn [_] "{\"a\":[\"b\"]}")]
       (is (= (read-collection "a.json")
