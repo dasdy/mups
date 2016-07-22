e0f7185126869a96cdf7a16726cca490233196c9
Podze Aleksandr <dasdeg@gmail.com>
2016-07-22 11:58:41 +0300

    update libs

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [zsau/id3 "0.1.1"]
-                 [cheshire "5.6.1"]
+                 [cheshire "5.6.3"]
                  [claudio "0.1.3"]
-                 [http-kit "2.1.19"]
-                 [clj-http "3.0.1"]
-                 [org.clojure/tools.cli "0.3.3"]]
+                 [http-kit "2.2.0"]
+                 [clj-http "3.1.0"]
+                 [org.clojure/tools.cli "0.3.5"]]
   :main ^:skip-aot mp3-update-scanner.core
   :target-path "target/%s"
   :resource-paths ["resources"]
