1891ba51628685564eeb543a94085852346f9d14
Podze Aleksandr <dasdeg@gmail.com>
2017-04-16 07:21:23 -0700

    preparations for creating web server

--- project.clj
+++ project.clj
@@ -5,12 +5,18 @@
             :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [zsau/id3 "0.1.1"]
-                 [cheshire "5.6.3"]
+                 [cheshire "5.7.0"]
                  [claudio "0.1.3"]
                  [http-kit "2.2.0"]
-                 [clj-http "3.1.0"]
-                 [org.clojure/tools.cli "0.3.5"]]
+                 [clj-http "3.5.0"]
+                 [compojure "1.5.2"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [selmer "1.10.7"]
+                 [prone "1.1.4"]]
   :main ^:skip-aot mp3-update-scanner.core
   :target-path "target/%s"
   :resource-paths ["resources"]
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all
+                       :uberjar-name "mups.jar"}
+             :dev {:ring {:stacktrace-middleware prone.middleware/wrap-exceptions}}}
+  :plugins [[lein-ring "0.11.0"]])
