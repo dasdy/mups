9201fba16da8c9ca7081b18318df7ee8b4a57b76
Podze Aleksandr <dasdeg@gmail.com>
2017-10-25 15:11:20 +0300

    removed unneeded libraries

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mups "0.1.0-SNAPSHOT"
+(defproject mups "0.2.0"
   :description "scan your media library; find if there are new albums by the artists you listen to"
   :url "https://github.com/dasdy/mups"
   :license {:name "MIT License"
@@ -8,11 +8,7 @@
                  [cheshire "5.8.0"]
                  [claudio "0.1.3"]
                  [http-kit "2.2.0"]
-                 [clj-http "3.7.0"]
-                 [compojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [selmer "1.11.1"]
-                 [prone "1.1.4"]
                  [swiss-arrows "1.0.0"]
                  [hiccup "1.0.5"]]
   :main mups.cli
@@ -21,6 +17,5 @@
   :resource-paths ["resources"]
   :jvm-opts ["-Xmx1g"]
   :profiles {:uberjar {:aot :all
-                       :uberjar-name "mups.jar"}
-             :dev {:ring {:stacktrace-middleware prone.middleware/wrap-exceptions}}}
+                       :uberjar-name "mups.jar"}}
   :plugins [[lein-ring "0.12.1"]])
