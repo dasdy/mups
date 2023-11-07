6d3829fa87ce3a88a275b42af215cbac6d92e29d
Sasha Podze <sasha.podze@people.ai>
2023-11-07 16:10:21 +0100

    update some deps

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/dasdy/mups"
   :license {:name "MIT License"
             :url "https://mit-license.org/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cheshire "5.8.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [cheshire "5.12.0"]
                  [claudio "0.1.3"]
-                 [http-kit "2.2.0"]
-                 [org.clojure/tools.cli "0.3.5"]
-                 [mock-clj "0.1.0"]
+                 [http-kit "2.7.0"]
+                 [org.clojure/tools.cli "1.0.219"]
+                 [mock-clj "0.2.1"]
                  [hiccup "1.0.5"]]
   :main mups.cli
   :aot [mups.cli]
@@ -17,4 +17,4 @@
   :jvm-opts ["-Xmx1g"]
   :profiles {:uberjar {:aot :all
                        :uberjar-name "mups.jar"}}
-  :plugins [[lein-ring "0.12.1"]])
+  :plugins [[lein-ring "0.12.1"] [lein-ancient "1.0.0-RC3"]])
